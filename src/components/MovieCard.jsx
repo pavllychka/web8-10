f2db181f4698775cf66409dc0ebe7755f4f5a847
sonichka <spavlyk7@gmail.com>
2025-05-13 21:27:27 +0300

    оновлюю тим шо додала зоображення плюс жанр і сеанс

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 const MovieCard = ({ movie }) => {
   return (
-    <div>
+    <div className="movie-card">
+      <img src={movie.poster} alt={`${movie.title} постер`} />
       <h3>{movie.title}</h3>
       <p>{movie.description}</p>
+      <p><strong>Жанр:</strong> {movie.genre}</p>
+      <p><strong>Сеанс:</strong> {movie.showtime}</p>
     </div>
   );
 };
