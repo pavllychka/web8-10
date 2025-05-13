0a503ecd3755295281fbecbe16cc829a0b007bcb
sonichka <spavlyk7@gmail.com>
2025-05-13 21:24:33 +0300

    Додаю movies.js заповненим  фільмами

---
+++ src/data/movies.js
@@ -0,0 +1,44 @@
+const movies = [
+    {
+      id: 1,
+      title: "Початок",
+      description: "Злодій, який проникає в сни, виконує складне завдання.",
+      genre: "Фантастика",
+      poster: "https://images.prom.ua/4222523998_w640_h640_4222523998.jpg",
+      showtime: "2025-05-15 19:00",
+    },
+    {
+      id: 2,
+      title: "Втеча з Шоушенка",
+      description: "Двоє ув'язнених знаходять надію через дружбу.",
+      genre: "Драма",
+      poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3x5L689-Sh-e2DrlCFDFwg_70a_6FXEc62A&s",
+      showtime: "2025-05-15 21:00",
+    },
+    {
+        id: 3,
+        title: "Майнкрафт",
+        description: "Фільм заснований на всіма відомій грі майнкрафт.",
+        genre: "Пригоди",
+        poster: "https://cdn.planetakino.ua/4612_minecraft_2025/Media/Posters/vertical/52d15a99-deab-48c6-9512-8af553e41a5b.jpg",
+        showtime: "2025-05-15 15:00",
+    },
+    {
+        id: 4,
+        title: "Громовержці",
+        description: "Всі стріляють і нападають",
+        genre: "Екшн",
+        poster: "https://cdn.planetakino.ua/568_thunderbolts_2025/Media/Posters/vertical/opt_5b792940-c02a-46bf-8c93-8e60146ac064.webp",
+        showtime: "2025-05-15 13:00",
+    },
+    {
+        id: 5,
+        title: "Шалені копи",
+        description: "ДУ поліцейському відділку маленького острівного містечка поповнення – Вілл Шеллі.",
+        genre: "Кримінальне",
+        poster: "https://cdn.planetakino.ua/14540_greedy-people_2024/Media/Posters/vertical/opt_67f8ce41-21ed-479e-a8d3-01143b3501c8.webp",
+        showtime: "2025-05-15 12:00",
+    },    
+  ];
+  
+  export default movies;
